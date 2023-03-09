0cbc80f4587fd1e9004838e5453876e32c73e124
JoelPrz990 <jopeor.2000@gmail.com>
2023-03-09 03:00:57 -0600

    wuuuuu

--- WindowsFormsApp1/actualizarList/frmACategoria.Designer.cs
+++ WindowsFormsApp1/actualizarList/frmACategoria.Designer.cs
@@ -28,20 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvCategoria = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboxCategoria = new System.Windows.Forms.ComboBox();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvCategoria);
+            this.groupBox2.Location = new System.Drawing.Point(13, 242);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1003, 281);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Categorias Disponibles";
+            // 
+            // dgvCategoria
+            // 
+            this.dgvCategoria.AllowUserToAddRows = false;
+            this.dgvCategoria.AllowUserToDeleteRows = false;
+            this.dgvCategoria.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCategoria.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dgvCategoria.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dgvCategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategoria.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCategoria.Location = new System.Drawing.Point(3, 18);
+            this.dgvCategoria.Name = "dgvCategoria";
+            this.dgvCategoria.ReadOnly = true;
+            this.dgvCategoria.RowHeadersWidth = 51;
+            this.dgvCategoria.RowTemplate.Height = 24;
+            this.dgvCategoria.Size = new System.Drawing.Size(997, 260);
+            this.dgvCategoria.TabIndex = 0;
+            this.dgvCategoria.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.cboxCategoria);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.btnAceptar);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1004, 223);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(631, 177);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(102, 40);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(472, 177);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(102, 40);
+            this.btnAceptar.TabIndex = 3;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNombre.Location = new System.Drawing.Point(461, 124);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(383, 22);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F);
+            this.label1.Location = new System.Drawing.Point(24, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Agregar nueva categoria";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nuevo nombre de la categoria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(342, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombre de la categoria por actualizar";
+            // 
+            // cboxCategoria
+            // 
+            this.cboxCategoria.FormattingEnabled = true;
+            this.cboxCategoria.Location = new System.Drawing.Point(461, 75);
+            this.cboxCategoria.Name = "cboxCategoria";
+            this.cboxCategoria.Size = new System.Drawing.Size(383, 24);
+            this.cboxCategoria.TabIndex = 6;
+            this.cboxCategoria.Leave += new System.EventHandler(this.cboxCategoria_Leave);
             // 
             // frmACategoria
             // 
+            this.AcceptButton = this.btnAceptar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Linen;
+            this.CancelButton = this.btnCancelar;
             this.ClientSize = new System.Drawing.Size(1028, 535);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "frmACategoria";
             this.Text = "frmACategoria";
+            this.Enter += new System.EventHandler(this.frmACategoria_Enter);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoria)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvCategoria;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboxCategoria;
     }
 }
--- WindowsFormsApp1/actualizarList/frmACategoria.cs
+++ WindowsFormsApp1/actualizarList/frmACategoria.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using BOL.Categoria;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,60 @@ namespace WindowsFormsApp1.actualizarList
 {
     public partial class frmACategoria : Form
     {
+        CategoriaBLL categoriaBLL = CategoriaBLL.Instance();
+        int idCategoria;
         public frmACategoria()
         {
             InitializeComponent();
+        }
+
+
+        private void frmACategoria_Enter(object sender, EventArgs e)
+        {
+            reload();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            idCategoria = categoriaBLL.getByDescripcion(new Categoria()
+            {
+                Nombre_Categoria = cboxCategoria.Text
+            }).ID_Categoria;
+
+            if (categoriaBLL.Update(new Categoria()
+            {
+                ID_Categoria = idCategoria,
+                Nombre_Categoria = txtNombre.Text
+            }))
+            {
+                MessageBox.Show("Categoria Eliminada Correctamente", "FerroPapus",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                reload();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Campos limpiados", "FerroPapus",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+            cboxCategoria.SelectedIndex = -1;
+        }
+        public void reload() {
+            Categorias categorias = categoriaBLL.GetAll();
+            foreach (Categoria categoria in categorias)
+            {
+                cboxCategoria.Items.Add(categoria.Nombre_Categoria);
+            }
+            txtNombre.Clear();
+            cboxCategoria.SelectedIndex = -1;
+            dgvCategoria.DataSource = categoriaBLL.GetAll();
+            dgvCategoria.Columns["ID_Categoria"].Visible = false;
+            dgvCategoria.Columns["activo"].Visible = false;
+        }
+
+        private void cboxCategoria_Leave(object sender, EventArgs e)
+        {
+            txtNombre.Text = cboxCategoria.Text;
         }
     }
 }
